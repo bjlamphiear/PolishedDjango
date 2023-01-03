0ffa869dcf6c48e52c1a33aac98a5afa6d05cd6a
Brian Lamphiear <bjlamphiear@gmail.com>
2023-01-03 06:39:48 -0800

    slow caroselle

--- static/website/js/default-assets/active.js
+++ static/website/js/default-assets/active.js
@@ -30,7 +30,7 @@
       items: 1,
       loop: true,
       autoplay: true,
-      smartSpeed: 1500,
+      smartSpeed: 2500,
       nav: true,
       navText: ["<i class='ti-angle-left'</i>", "<i class='ti-angle-right'</i>"],
       dots: true,
